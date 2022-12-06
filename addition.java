9ad8cf1be21bb011ec34cfb878e326335897650d
vishnulaskari <102209982+vishnulaskari@users.noreply.github.com>
2022-12-06 15:08:35 +0530

    addition and calculator program

---
+++ addition.java
@@ -0,0 +1,15 @@
+
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		int num1 = 20;
+		int num2 = 30;
+		int total = num1 +num2;
+		
+		
+		
+		System.out.println(total);
+
+	}
+
+}
